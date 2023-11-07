3a138d450d478d85d4cc6976bc6c39ded653f147
NguyenXuanTrinh <trinh.nguyen2412@hcmut.edu.vn>
2023-11-07 20:16:51 +0700

    finish login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import * as userController from "../controllers/userController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", userController.findAll);
-router.get("/hi", userController.sayHi);
+router.get("/", verifyToken, userController.findAll);
+router.post("/login", userController.login)
 
 export default router;
