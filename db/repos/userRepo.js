e07e77b67b1da57e620bf5ac56e5f1dcaa4c1041
Le Cao Minh Tri <103999209+LCMTRI@users.noreply.github.com>
2023-11-09 16:07:37 +0700

    feat (authentication): add signup api

--- db/repos/userRepo.js
+++ db/repos/userRepo.js
@@ -1,15 +1,19 @@
 import { UserModel } from "../../models/userModel.js";
 
 async function retrieveAll() {
-    return UserModel.find({});
+	return UserModel.find({});
 }
 
 async function findByPhoneNumber(phoneNumber) {
-    return UserModel.findOne({'phone': phoneNumber})
+	return UserModel.findOne({ 'phone': phoneNumber });
 }
 
+async function createUser(User) {
+	return UserModel.create(User);
+}
 
 export default {
-    retrieveAll,
-    findByPhoneNumber
+	retrieveAll,
+	findByPhoneNumber,
+	createUser
 }
