8e1f9f3e187f7c6e21a6d65b941daee0befc98d8
quang2303 <chungducquang2@gmail.com>
2023-11-10 22:32:14 +0700

    create api createAppointment

--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -4,5 +4,6 @@ import * as appointmentController from "../controllers/appointmentController.js"
 const router = express.Router();
 
 router.get("/:username", appointmentController.findAllByUsername);
+router.get("/createappointment", appointmentController.createAppointment);
 
 export default router;
