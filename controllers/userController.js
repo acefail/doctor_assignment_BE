18e34cbae8199eda404b708491a05bd917d083f9
quang2303 <chungducquang2@gmail.com>
2023-12-03 22:11:35 +0700

    Update controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,8 +10,6 @@ export const findAll = async (req, res, next) => {
   });
 };
 
-export const aaaa = async (req, res, next) => {};
-
 export const findInfoByUsername = async (req, res, next) => {
   const { phoneNumber } = req.params;
   const user = await userRepo.findByPhoneNumber(phoneNumber);
